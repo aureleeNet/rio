e8c4750f42f0c686b8a52f9a317b9d2ed253bb6e
Alexander Steen <a.steen@fu-berlin.de>
2021-05-10 17:42:13 +0200

    simplified implementation of Out4 based on reduction to Out2

--- src/main/scala/net/aurelee/rio/reasoner/Out4.scala
+++ src/main/scala/net/aurelee/rio/reasoner/Out4.scala
@@ -1,23 +1,11 @@
 package net.aurelee.rio.reasoner
 
-import net.aurelee.rio.core.{Formula, Norm, OutOperator, PLTop, interreduce, mkConjs}
-import net.aurelee.rio.sat.consequence
+import net.aurelee.rio.core.{Formula, Norm, OutOperator}
 
 object Out4 extends OutOperator {
   override final def name: String = "out4"
   override final def apply(norms: Seq[Norm], input: Seq[Formula], throughput: Boolean): Seq[Formula] = {
     if (throughput) Out2.apply(norms, input, throughput)
-    else {
-      var lastOutput: Seq[Formula] = Seq(PLTop)
-      var basicOutput = Out2.apply(norms, input, throughput)
-      var triggeredOutput = basicOutput.concat(Out3.apply(norms, input.concat(basicOutput), throughput))
-
-      while (!consequence(lastOutput, mkConjs(triggeredOutput))) {
-        lastOutput = triggeredOutput
-        basicOutput = triggeredOutput.concat(Out2.apply(norms, lastOutput, throughput))
-        triggeredOutput = basicOutput.concat(Out3.apply(norms, input.concat(basicOutput), throughput))
-      }
-      interreduce(lastOutput)
-    }
+    else Out2.apply(norms, input.concat(materialization(norms)), throughput)
   }
 }
