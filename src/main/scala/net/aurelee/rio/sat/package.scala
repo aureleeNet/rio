b97b09f7c9b436294f3ef352fd36a49ddd825e5e
Alexander Steen <a.steen@fu-berlin.de>
2022-08-25 21:02:18 +0200

    update

--- src/main/scala/net/aurelee/rio/sat/package.scala
+++ src/main/scala/net/aurelee/rio/sat/package.scala
@@ -1,11 +1,23 @@
 package net.aurelee.rio
 
-import net.aurelee.rio.core.Formula
+import net.aurelee.rio.core.{Formula, PLNeg, PLProp, mkConjs, mkDisjs}
+
+import java.io.File
+import scala.collection.mutable
+import scala.sys.process.ProcessLogger
 
 package object sat {
   implicit lazy val globalPicoSATInstance: PicoSAT = PicoSAT.get()
+  final lazy val musToolFile: String = {
+    val must = getClass.getResourceAsStream("/must")
+    val target = java.nio.file.Files.createTempFile("riomust", "")
+    java.nio.file.Files.copy(must, target, java.nio.file.StandardCopyOption.REPLACE_EXISTING)
+    target.toFile.setExecutable(true)
+    target.toFile.deleteOnExit()
+    target.toAbsolutePath.toString
+  }
 
-  final def cnfToPicoSATClauses(cnf: Formula): Seq[Seq[Int]] = {
+  final def cnfToDimacsClauses(symbolMap: Option[Map[String, Int]])(cnf: Formula): Seq[Seq[Int]] = {
     import net.aurelee.rio.core.{PLNeg, PLProp, PLTop, PLBottom}
     import scala.collection.mutable
 
@@ -14,18 +26,38 @@ package object sat {
     else {
       val result: mutable.ListBuffer[Seq[Int]] = mutable.ListBuffer.empty
       val clauses = cnf.conjs
-      val symbols = cnf.symbols.toVector
+      val namingMap = symbolMap match {
+        case Some(value) => value
+        case None => cnf.symbols.zipWithIndex.toMap
+      }
       clauses.foreach { cl =>
         val lits = cl.disjs
         val translatedLits = lits.map { l => (l: @unchecked) match {
-          case PLNeg(PLProp(name)) => -(symbols.indexOf(name)+1)
-          case PLProp(name) => symbols.indexOf(name)+1
+          case PLNeg(PLProp(name)) => -(namingMap(name)+1)
+          case PLProp(name) => namingMap(name)+1
         }
         }
         result.append(translatedLits)
       }
       result.toSeq
     }
+  }
+
+  final def cnfToDimacsProblem(symbolMap: Option[Map[String, Int]])(cnf: Formula): String = {
+    val sb: mutable.StringBuilder = new mutable.StringBuilder()
+    val namingMap = symbolMap match {
+      case Some(value) => value
+      case None => cnf.symbols.zipWithIndex.toMap
+    }
+    val dimacsClauses = cnfToDimacsClauses(Some(namingMap))(cnf)
+    val varCount = cnf.symbols.size
+    val clauseCount = dimacsClauses.size
+    sb.append(s"p cnf $varCount $clauseCount\n")
+    dimacsClauses foreach { clause =>
+      sb.append(clause.mkString(" "))
+      sb.append(s" 0\n")
+    }
+    sb.init.toString // drop last newline
   }
 
 
@@ -62,11 +94,63 @@ package object sat {
     val clausified = cnf(formula)
     val simplified = simp(clausified)
 //    println(s"[PicoSAT] Check ${simplified.pretty}")
-    val picoSatInput = cnfToPicoSATClauses(simplified)
+    val picoSatInput = cnfToDimacsClauses(None)(simplified)
     solver.reset()
     picoSatInput.foreach(solver.addClause)
     solver.solve()
   }
 
+  final def allMUSes(formula: Formula)(implicit solver: PicoSAT): Seq[Formula] = {
+    import net.aurelee.rio.core.{cnf, simp}
+    val clausified = cnf(formula)
+    val simplified = simp(clausified)
+    // translate to Dimacs
+    val symbolMap = simplified.symbols.zipWithIndex.toMap
+    val reverseMap = symbolMap.map(_.swap) // works because symbolMap is injective
+    val dimacs = cnfToDimacsProblem(Some(symbolMap))(simplified)
+    // run MUS enumeration tool
+    val mustResult = runMUST(dimacs)
+    val muses = MUSesFromDimacs(mustResult)
+    // translate result file to Seq[Formula]
+    val result: Seq[Formula] = muses.map { mus =>
+      val clauses = mus.map { clause =>
+        val cl: Seq[Formula] = clause.map { lit =>
+          (lit: @unchecked) match {
+            case i if i > 0 => PLProp(reverseMap(i - 1))
+            case i if i < 0 => PLNeg(PLProp(reverseMap(-i - 1)))
+          }
+        }
+        mkDisjs(cl)
+      }
+      mkConjs(clauses)
+    }
+    result
+  }
+  private[this] final def runMUST(dimacs: String): String = {
+    import java.nio.file.Files
+    // write dimacs file to temporary file
+    val tempDimacsFile = Files.createTempFile("rio", ".cnf")
+        tempDimacsFile.toFile.deleteOnExit()
+    val filewriter = Files.newBufferedWriter(tempDimacsFile)
+    filewriter.write(dimacs)
+    filewriter.flush(); filewriter.close()
+    // construct output file
+    val tempOutputFile = Files.createTempFile("rio", ".musoutput")
+        tempOutputFile.toFile.deleteOnExit()
+    // call MUST with input, wait for termination
+    val process = scala.sys.process.Process.apply(musToolFile, Seq(tempDimacsFile.toAbsolutePath.toString, "-o", tempOutputFile.toAbsolutePath.toString))
+    process ! ProcessLogger(line => (), err => println(err)) // ignore stdout
+    // read result file
+    val toolOutput = Files.readString(tempOutputFile)
+    toolOutput
+  }
+  private[this] final def MUSesFromDimacs(musDimacs: String): Seq[Seq[Seq[Int]]] = {
+    val musEntries = musDimacs.split("\n\n")
+    musEntries.map{ entry =>
+      val lines = entry.split("\n")
+      val clauses = lines.tail.tail.toSeq // drop first two lines: "MUS #i" and "p cnf k j"
+      clauses.map(cl => cl.split(" ").init.map(_.toInt).toSeq)
+    }
+  }
 
 }
